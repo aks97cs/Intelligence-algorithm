223c1e5de24b81a3344d9967886c055b9a992954
= <aks97cs@gmail.com>
2020-09-08 07:27:42 +0530

    gradient termination logic

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -1,9 +1,13 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+
 x_label = [2, 4, 5, 9]  # feature
 y_label = [1.2, 2.8, 5.3, 9]  # target
 
 """
 Hypothesis:
-h = theta0 + theta1*x_label
+h = theta0 + theta1*x_labRel
 """
 
 
@@ -22,9 +26,10 @@ def cost_function(theta0, theta1, h_label):
 
 
 def gradient_descent():
-    alpha = 0.00000000000001  # learning rate
+    alpha = 0.0000000000000001  # learning rate
     theta0, theta1 = 0, 1  # initial coefficient value
     count = 1
+    error = []
     while True:
         print(f"==================epoch = {count} ============")
         print(f"for theta0 = {theta0} and theta1 = {theta1}")
@@ -37,10 +42,13 @@ def gradient_descent():
             j_theta0 += -2 * (y_label[el] - h_label[el])
             j_theta1 += -2 * x_label[el] * (y_label[el] - h_label[el])
         theta0, theta1 = (theta0 - (alpha * j_theta0)), (theta1 - (alpha * j_theta1))
-        if j_theta1+j_theta0 == 0:
+        if count == 100000:
             break
+        error.append(_cost)
         count += 1
+    print("Error: ", min(error))
 
 
 if __name__ == "__main__":
     gradient_descent()
+   
